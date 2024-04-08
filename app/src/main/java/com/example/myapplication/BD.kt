b968790f7a0a65e6bb198d5e32ebc422c4d18c4a
THAIS_BRASILEIRO_TORRES <72201266@alfab35.barroca>
2024-04-08 20:06:01 -0300

    commit

--- app/src/main/java/com/example/myapplication/BD.kt
+++ app/src/main/java/com/example/myapplication/BD.kt
@@ -5,13 +5,5 @@ class BD {
     companion object{
         var users: ArrayList<UserModel> = ArrayList()
 
-        fun preencheUsers(){
-            var user = UserModel()
-            user.nome = "Thais"
-            user.username = "admin"
-            user.senha = "123456"
-
-            users.add(user)
-        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/CadastroActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        val btVoltar: ImageView = findViewById<ImageView>(R.id.btVoltar)
+        val btCadastrar: Button = findViewById<Button>(R.id.btCadCadastrar)
+
+        btVoltar.setOnClickListener{
+            finish()
+        }
+
+        btCadastrar.setOnClickListener{
+
+            val etNome = findViewById<EditText>(R.id.etCadNome)
+            val etUsuario = findViewById<EditText>(R.id.etCadUsuario)
+            val etSenha = findViewById<EditText>(R.id.etCadSenha)
+            val etConfSenha = findViewById<EditText>(R.id.etCadConfirmeSenha)
+
+            if (etUsuario.text.toString() != "" && etSenha.text.toString() != "" && etNome.text.toString() != "" && etConfSenha.text.toString() != "") {
+
+                if (etSenha.text.toString() == etConfSenha.text.toString()){
+                    var user = UserModel()
+                    user.nome = etNome.text.toString()
+                    user.username = etUsuario.text.toString()
+                    user.senha = etSenha.text.toString()
+
+                    BD.users.add(user)
+
+                }else{
+                    Toast.makeText(this, "As senhas não conferem", Toast.LENGTH_SHORT).show()
+                }
+
+            }else{
+                Toast.makeText(this, "Digite todos os campos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
